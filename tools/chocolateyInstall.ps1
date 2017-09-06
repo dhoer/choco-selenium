d332147ddb454279776a5a45a847c3d598a4cfd4
Dennis Hoer <dennis.hoer@pearson.com>
2017-09-06 16:51:05 -0700

    Ensure cap is an array

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -28,6 +28,7 @@ if ($pp["role"] -eq 'node') {
   }
   # https://stackoverflow.com/a/38212718/4548096
   $pp["capabilities"] = Get-Content -Path $pp["capabilitiesJson"] | ConvertFrom-Json | % { $_ }
+  if ($pp["capabilities"] -isnot [Array]) { $pp["capabilities"] = @($pp["capabilities"]) }
 }
 
 if ($pp["log"] -ne $null -and $pp["log"] -ne '' -and !(Test-Path $pp["log"])) {
