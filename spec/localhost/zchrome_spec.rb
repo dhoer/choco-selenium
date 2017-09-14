3f85e48750eca83146cb9dc572dfdd2825af36e4
Dennis Hoer <dennis.hoer@pearson.com>
2017-09-14 08:11:41 -0700

    Tweak webdriver spec

--- spec/localhost/zchrome_spec.rb
+++ spec/localhost/zchrome_spec.rb
@@ -3,17 +3,16 @@ require 'spec_helper'
 describe 'Google Chrome' do
   before(:all) do
     @selenium = Selenium::WebDriver.for(:remote, url: "http://localhost:4446/wd/hub", desired_capabilities: :chrome)
+    @resolution = '1024 x 768'
   end
 
   after(:all) do
     @selenium.quit
   end
 
-  res = '1024 x 768'
-
-  it "should return display resolution of #{res}" do
+  it "should return display resolution of #{@resolution}" do
     @selenium.get 'http://www.whatismyscreenresolution.com/'
     element = @selenium.find_element(:id, 'resolutionNumber')
-    expect(element.text).to eq(res)
+    expect(element.text).to eq(@resolution)
   end
 end
