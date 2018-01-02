425bcff4fbf3439c1d8b55980406e0114197a089
Dennis Hoer <dennis.hoer@pearson.com>
2018-01-01 17:25:48 -0700

    Add java tests

---
+++ src/test/java/ChromeTest.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ChromeTest {
+
+  private WebDriver driver;
+
+  @BeforeTest
+  public void beforeTest() throws MalformedURLException {
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setBrowserName("chrome");
+
+    driver = new RemoteWebDriver(new URL("http://localhost:4446/wd/hub"), capabilities);
+    driver.get("http://www.google.com");
+  }
+
+  @Test
+  public void main() {
+    WebElement element = driver.findElement(By.name("q"));
+    element.sendKeys("Cheese!");
+    element.submit();
+
+    (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+      public Boolean apply(WebDriver d) {
+        return d.getTitle().toLowerCase().startsWith("cheese!");
+      }
+    });
+  }
+
+  @AfterTest
+  public void afterTest() {
+    driver.quit();
+  }
+}
---
+++ src/test/java/EdgeTest.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class EdgeTest {
+
+  private WebDriver driver;
+
+  @BeforeTest
+  public void beforeTest() throws MalformedURLException {
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setBrowserName("MicrosoftEdge");
+
+    driver = new RemoteWebDriver(new URL("http://localhost:4446/wd/hub"), capabilities);
+    driver.get("http://www.google.com");
+  }
+
+  @Test
+  public void main() {
+    WebElement element = driver.findElement(By.name("q"));
+    element.sendKeys("Cheese!");
+    element.submit();
+
+    (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+      public Boolean apply(WebDriver d) {
+        return d.getTitle().toLowerCase().startsWith("cheese!");
+      }
+    });
+  }
+
+  @AfterTest
+  public void afterTest() {
+    driver.quit();
+  }
+}
